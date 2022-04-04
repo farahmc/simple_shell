266b9bbe83594df8d369fe6ebc96990f1e947b59
jashoney <4479@holbertonschool.com>
2022-04-04 11:37:38 +1000

    initial shell commit

---
+++ shell.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void sighandler(int sig_num)
+{
+	signal(SIGINT, sighandler);
+	if (sig_num == SIGINT)
+		write(1, "\n->$", 4);
+	fflush(stdout);
+}
+
+char *readaline(void)
+{
+	ssize_t read = 0;
+	ssize_t slen = 0;
+	char *buff = NULL;
+
+	read = getline(&buff, &slen, stdin);
+	if (read == -1)				/* Ctrl-D used */
+	{
+		free(buff);
+		exit(0);
+	}
+	if (buff[read - 1] == '\n')
+		buff[read - 1] = '\0';
+	return (buff);
+}
+
+int main(int ac, char **av)
+{
+	int strlen = 0, i = 0, j = 0, k = 0, status = 1, writeflag = 0;
+	ssize_t size = 200;
+	char *buffstring, *token = NULL, *argv[10], *path;
+	pid_t pid;
+
+	signal(SIGINT, sighandler);
+	path = getenv("PATH");
+	if (path == NULL)
+		return (1);
+	
+	while (i < 20)
+	{
+		j = 0;
+		if (isatty(STDIN_FILENO))
+		{
+			writeflag = write(1, "->$", 3);
+			if (writeflag == -1)
+				return (1);
+		}
+		buffstring = readaline();
+		token = strtok(buffstring, " ");
+		while (token != NULL)
+		{
+			argv[j] = token;
+			token = strtok(NULL, " ");
+			j++;
+		}
+		argv[j] = token;
+		pid = fork();
+		printf("i = %d, pid = %d\n", i, pid);
+		if (pid == -1)
+		{
+			perror("Error:");
+			return (-1);
+		}
+		if (pid == 0)
+		{
+			printf("in exec part, argv[0] = %s\n", argv[0]);
+			execve(argv[0], argv, NULL);
+		}
+		wait(&status);
+		if (pid != 0)
+			i++;
+		for (k = 0; k <= j; k++)
+			argv[k] = NULL;
+	}
+	return (0);
+}
