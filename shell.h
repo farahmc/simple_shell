7b83597f7f60dd3b4d3603ef1f78a5322ceadaaa
Karoline-S <karoline.amy@gmail.com>
2022-04-05 19:50:59 -0700

    update header file

--- shell.h
+++ shell.h
@@ -1,22 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 extern char **environ;
 
 char *file_path(char **argv);
-char *_strdup(char *s);
-int _strcmp(char *s1, char *s2);
-char *str_concat(char *s1, char *s2);
 char *_getenv(const char *name);
-void print_env(char *name);
 int builtins (char *string);
+int forkwaitexec(char **argv);
 int _cd(char *path);
 int _strlen(char *s);
 int _env();
+void print_env(char *name);
+void flushargv(char **argv, int size);
 
 #endif
