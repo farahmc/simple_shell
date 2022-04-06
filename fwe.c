7e295717fce1978e61d7e45e6ed56f01a8926590
Karoline-S <karoline.amy@gmail.com>
2022-04-05 22:49:28 -0700

    build - add code forkwaitexec

---
+++ fwe.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * forwaitexec - forks a child process to execute a command while parent process
+ * waits
+ * @argv: an array of arguments containing the pathname for the command and
+ * any command arguments
+ *
+ *Retur: 0 on success or 1 on fork failure
+ */
+int forkwaitexec(char **argv)
+{
+	int wstatus;
+	pid_t childpid;
+
+	childpid = fork();
+
+	if (childpid == -1)
+		return (1);
+
+	if (childpid == 0)
+		execve(argv[0], argv, NULL);
+
+	wait(&wstatus);
+
+	return (0);
+}
