209b76e4d6868a593a48939c6d31496826ea8cb6
farahmc <farah.mccurdy@gmail.com>
2022-04-05 20:30:35 -0700

    tested - working func, moved _strlen to separate file

--- _env.c
+++ _env.c
@@ -1,31 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "shell.h"
 
-int _strlen(char *s)
-{
-	int i;
-
-	if (s == NULL || *s == '\0')
-		return (0);
-
-	for (i = 0; *s != '\0'; s++)
-		i++;
-
-	return (i);
-}
-
-
-int _env(char *env) // is parameter right?
+int _env()
 {
 	int i = 0;
 
-	while (env[i] != NULL)
+	while (environ[i] != NULL)
 	{
-		write(STDOUT_FILENO, env[i], _strlen(env[i]));
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
