e446dd1f871201909c0c90db843ec1c41669b66d
Karoline-S <karoline.amy@gmail.com>
2022-04-05 21:53:33 -0700

    update header file to remove libraries

--- shell.h
+++ shell.h
@@ -3,7 +3,7 @@
 
 extern char **environ;
 
-char *file_path(char **argv);
+char *file_path(char *command);
 char *_getenv(const char *name);
 int builtins (char *string);
 int forkwaitexec(char **argv);
@@ -11,6 +11,6 @@ int _cd(char *path);
 int _strlen(char *s);
 int _env();
 void print_env(char *name);
-void flushargv(char **argv, int size);
+void flushargv(char **argv);
 
 #endif
