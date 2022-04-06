303ed6ee057eb8ec605094d319a0ceb0ba88ada6
Karoline-S <karoline.amy@gmail.com>
2022-04-05 21:48:38 -0700

    perf - use NULL entry not length, update prototype, remove printf

--- flushargv.c
+++ flushargv.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * flushargv - initialises pointers in an array of strings to NULL
  * @argv: the array of strings to be traversed
- * @size: the size of the array
+ *
  * Return: nothing
  */
-void flushargv(char **argv, int size)
+void flushargv(char **argv)
 {
 	int i = 0;
 
-	while (i < size)
+	while (argv[i] != NULL)
 	{
 		argv[i] = NULL;
 		i++;
