694017e147db23ada6c3f78a272d8fec67f48422
Karoline-S <karoline.amy@gmail.com>
2022-04-07 12:40:08 -0700

    perf - add new prototyples

--- shell.h
+++ shell.h
@@ -17,22 +17,22 @@ typedef struct list_path
 	struct list_path *next;
 } list_path;
 
+int buffertokens(char **argv, char *buffer);
+void freeargv(char **argv);
 char *file_path(char *command);
+int break_up_path(char *envpath, list_path **head);
+void free_pathlist(list_path *head);
 char *_getenv(const char *name);
-char *_strdup(char *string);
+void print_env(char *name);
+int forkwaitexec(char **argv);
 void _strcpy(char *dest, char *src);
+char *_strdup(char *string);
 int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
-int add_node_pathlist(list_path **head, char *string);
 int builtins (char *string);
-int forkwaitexec(char **argv);
-int _cd(char *path);
 int _env();
-int break_up_path(char *envpath, list_path **head);
-size_t print_pathlist(list_path *head);
-void print_env(char *name);
-void free_pathlist(list_path *head);
+int _cd(char *path);
 int error(char *firstarg);
 
 #endif
