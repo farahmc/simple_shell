3dd3f176f2eeee958e836ef8acea9cd8601f241c
jashoney <4479@holbertonschool.com>
2022-04-07 16:03:01 +1000

    removed string functions

--- builtins.c
+++ builtins.c
@@ -2,29 +2,6 @@
 #include "shell.h"
 
 /**
- * _strcmp- compare 2 strings
- * @s1: first string
- * @s2: second string to compare
- *
- * Description: compare each value between 2 strings
- * if the value is the same, return a 0
- * if the value is larger, return a positive number
- * if the value is smaller, return a negative number
- *
- * Return: the difference in value
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-
-	while (s1[i] == s2[i] && s1[i] != '\0')
-		i++;
-
-	return (s1[i] - s2[i]);
-}
-
-/**
  * builtins - compare input string to builtins
  * @string: string to compare
  *
@@ -38,15 +15,15 @@ int _strcmp(char *s1, char *s2)
 int builtins (char *string)
 {
 	if (_strcmp(string, "exit") == 0)
-		exit();
+		exit(1);;
 
 	if (_strcmp(string, "env") == 0)
 		_env();
 
 	if (_strcmp(string, "cd") == 0)
-		_cd();
+		_cd(string);
 
-	error();
+	error(string);
 	return (0);
 }
 
--- findpath.c
+++ findpath.c
@@ -5,58 +5,6 @@
 #include "shell.h"
 
 /**
- * _strcat - appends one string at the end of another
- * @dest: pointer to the string to be modified
- * @src: pointer to the string to be added
- * Return: returns  pointer dest (modified string)
- */
-char *_strcat(char *dest, char *src)
-{
-	int destlen, idx;
-
-	if (dest == NULL)
-		return (NULL);
-
-	if (src == NULL || *src == '\0')
-		return (dest);
-
-	destlen = _strlen(dest);
-
-	idx = 0;
-	while (src[idx] != '\0')
-	{
-		dest[destlen + idx] = src[idx];
-		idx++;
-	}
-
-	dest[destlen + idx] = '\0';
-
-	return (dest);
-}
-
-/**
- * _strcpy - copies string at src into memory at dest
- * @dest: pointer to the space for string
- * @src: pointer to string to be copied
- * Return: nothing
- */
-void _strcpy(char *dest, char *src)
-{
-	int i;
-
-	if (src == NULL || *src == '\0')
-		return;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-}
-
-/**
  * file_path - breaks $PATH into tokens to search for command in each dir
  * @command: a string containing the command
  *
