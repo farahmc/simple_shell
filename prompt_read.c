cd1461da6297f5e1d025ce0e24f641a9a6bed01b
Karoline-S <karoline.amy@gmail.com>
2022-04-07 16:19:49 -0700

    fix - add descriptions for all functions

--- prompt_read.c
+++ prompt_read.c
@@ -1,16 +1,31 @@
 #include "shell.h"
 
+/**
+ * sighandler - manages signal received by ctrl c
+ * @sig_num: the int representation of ctrl c
+ * Return: nothing
+ */
 void sighandler(int sig_num)
 {
 	if (sig_num == SIGINT)
 		write(STDOUT_FILENO, "\n$ ", 3);
 }
 
+/**
+ * prompt - writes a prompt to stdout
+ *
+ * Return: number of chars written or -1 for failure
+ */
 int prompt(void)
 {
 	return (write(STDOUT_FILENO, "$ ", 2));
 }
 
+/**
+ * readaline - reads a line from stdin to a buffer
+ *
+ * Return: pointer to the buffer containing input
+ */
 char *readaline(void)
 {
 	int  read;
@@ -31,6 +46,12 @@ char *readaline(void)
 	return (buffer);
 }
 
+/**
+ * error - prints error messages to the screen
+ * @firstarg: a string containing first argument passed in to
+ * the program
+ * Return: 0 for success or 1 for failure to write
+ */
 int error(char *firstarg)
 {
 	if (write(STDOUT_FILENO, firstarg, _strlen(firstarg)) == -1)
