b075f253fbc524644d9c13ca6d38d1ce2a74d57a
Karoline-S <karoline.amy@gmail.com>
2022-04-06 20:36:20 -0700

    docs - add new pathlist prototypes and struct declaration

--- shell.h
+++ shell.h
@@ -9,8 +9,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 extern char **environ;
+
+typedef struct list_path
+{
+	char *pathtoken;
+	struct list_path *next;
+} list_path;
 
 char *file_path(char *command);
 char *_getenv(const char *name);
@@ -19,7 +24,9 @@ int forkwaitexec(char **argv);
 int _cd(char *path);
 int _strlen(char *s);
 int _env();
+int break_up_path(char *envpath, list_path **head);
 void print_env(char *name);
 void flushargv(char **argv);
+void free_pathlist(list_path *head);
 
 #endif
