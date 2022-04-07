a9884e9c162b26fd999f97cfe61953caf753d95e
Karoline-S <karoline.amy@gmail.com>
2022-04-06 22:18:10 -0700

    add new prototypes

--- shell.h
+++ shell.h
@@ -19,12 +19,15 @@ typedef struct list_path
 
 char *file_path(char *command);
 char *_getenv(const char *name);
+char *_strdup(char *string);
+int add_node_pathlist(list_path **head, char *string);
 int builtins (char *string);
 int forkwaitexec(char **argv);
 int _cd(char *path);
 int _strlen(char *s);
 int _env();
 int break_up_path(char *envpath, list_path **head);
+size_t print_pathlist(list_path *head);
 void print_env(char *name);
 void flushargv(char **argv);
 void free_pathlist(list_path *head);
