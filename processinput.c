80e72ce840373d3eb1a718def97dff77cb806e93
Karoline-S <karoline.amy@gmail.com>
2022-04-08 14:40:58 -0700

    add builtins functionality

--- processinput.c
+++ processinput.c
@@ -8,39 +8,49 @@
  */
 int processinput(char *buffer)
 {
-	char *errorstring, *path, *argv[20];
+	char *errorstring = NULL, *path, *argv[20];
 
 	if (buffertokens(argv, buffer) == 1)
 		return (1);
 
-	errorstring = _strdup(argv[0]);
-
-	if (path_given(argv[0]) == 0)
+	while (1)
 	{
-		path = findpath(argv[0], errorstring);
-		if (path == NULL)
+		if (builtins(argv) == 0)
+			break;
+
+		if (*argv[0] == '/')
 		{
-			free(errorstring);
-			freeargv(argv);
-			return (1);
+			if (checkpath(argv[0]) == -1)
+				break;
 		}
 		else
 		{
-			free(argv[0]);
-			argv[0] = path;
+			errorstring = _strdup(argv[0]);
+			path = findpath(argv[0], errorstring);
+			if (path == NULL)
+			{
+				free(errorstring);
+				freeargv(argv);
+				return (1);
+			}
+			else
+			{
+				free(argv[0]);
+				argv[0] = path;
+			}
+
+			if (checkpath(argv[0]) == -1)
+				break;
 		}
+		break;
 	}
 
-	if (*argv[0] != '/' || checkpath(argv[0]) == 1)
-	{
-		errormessage(errorstring);
-	}
-	else
-	{
-		forkwaitexec(argv);
+	forkwaitexec(argv);
+
+	if (errorstring != NULL)
 		free(errorstring);
-	}
 
 	freeargv(argv);
+
 	return (0);
 }
