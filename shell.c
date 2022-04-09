76488653af619630b167f55ebbf338604b588c68
Karoline-S <karoline.amy@gmail.com>
2022-04-09 00:29:07 -0700

    fix - add argv to main declaration and process function call

--- shell.c
+++ shell.c
@@ -7,24 +7,33 @@
  */
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	char *buffstring;
 
 	signal(SIGINT, sighandler);
+
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 		{
 			if (prompt() == -1)
+			{
+				errormessage(argv[0]);
 				return (1);
+			}
 		}
 
 		buffstring = readaline();
-		if (buffstring == NULL)
-			return (1);
 
-		processinput(buffstring);
+		if (buffstring == NULL)
+		{
+			errormessage(argv[0]);
+			return (1);
+		}
+
+		if (processinput(buffstring, argc, argv) == 1)
+			return (1);
 	}
 
 	return (0);
