8b9528ac78f72387d29c889db8ea5ffeba364976
jashoney <4479@holbertonschool.com>
2022-04-12 13:51:15 +1000

    added an error_cd function (unused)

--- error_print.c
+++ error_print.c
@@ -38,3 +38,41 @@ void error_print(char *progname, char *command, int cmdcount)
 	if (isatty(STDIN_FILENO))
 		write(STDIN_FILENO, "\n", 1);
 }
+
+/**
+ * error_print - prints an error msg
+ * @progname: is the argv[0] on call
+ * @command: is the inputted shell command
+ * @cmdcount: number of commands entered so far
+ * Return: is void
+ */
+
+void error_cd(char *progname, char *command, int cmdcount)
+{
+        int proglen = 0, commlen = 0;
+        char digit[3];
+
+        proglen = _strlen(progname);
+        commlen = _strlen(command);
+        if (cmdcount < 10)
+        {
+                digit[0] = '0' + cmdcount;
+                digit[1] = '\0';
+        }
+        else
+        {
+                digit[0] = (cmdcount % 10) + '0';
+                digit[1] = (cmdcount / 10) + '0';
+                digit[2] = '\0';
+        }
+        write(STDOUT_FILENO, progname, proglen);
+        write(STDOUT_FILENO, ": ", 2);
+        if (cmdcount > 10)
+                write(STDOUT_FILENO, digit, 2);
+        else
+                write(STDOUT_FILENO, digit, 1);
+        write(STDOUT_FILENO, ": cd: can't cd to", 17);
+        write(STDOUT_FILENO, command, commlen);
+        if (isatty(STDIN_FILENO))
+                write(STDIN_FILENO, "\n", 1);
+}
